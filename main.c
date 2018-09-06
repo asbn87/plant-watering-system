865e4f90169669f7518a96b68690aea89df3b334
Andreas Nordin <andreas.nordin@rte.se>
2018-09-06 20:59:34 +0200

    lab-01-1: add solution
    Change-Id: Ieecd03c37c9fba5b8fb094b6d0976e6cd276ed16

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <avr/io.h>
+#include <util/delay.h>
 
 #include "serial.h"
 
+void led_init() {
+	DDRB = (1 << PORTB0);
+}
+
 void main (void) {
 
+	while (1) {
+		PORTB |= (1 << PORTB0);
+		_delay_ms(500);
+		PORTB &= ~(1 << PORTB0);
+		_delay_ms(500);
+	}
 }
