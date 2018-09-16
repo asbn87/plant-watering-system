721a0510fabea9062967a245175b6b7935a847e0
Andreas Nordin <andreas.nordin@rte.se>
2018-09-16 23:14:22 +0200

    Lab-03: baseline
    Change-Id: Ia1fba4dde4139ea94d6e9659741926d30fce9475

--- main.c
+++ main.c
@@ -1,16 +1,11 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
-
 #include <stdio.h>
 
 #include "led.h"
 #include "serial.h"
 #include "timer.h"
-
-ISR(TIMER2_COMPA_vect) {
-	OCR0A = simple_ramp();
-}
 
 void main (void) {
 
@@ -18,10 +13,8 @@ void main (void) {
 	uart_init();
 	timer_init();
 
-	sei();
-
 	while (1) {
-		;
+		// ...
 	}
 }
 
